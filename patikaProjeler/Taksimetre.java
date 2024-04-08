bf259e52d6a042727c9fa1651363ee6b7015e230
üzeyir <barbaross_61@hotmail.com>
2024-04-09 00:42:03 +0300

    1

---
+++ patikaProjeler/Taksimetre.java
@@ -0,0 +1,22 @@
+package patikaProjeler;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+
+    public static void main(String[] args) {
+        
+        Scanner inp = new Scanner(System.in);
+
+        double kmUcreti = 2.20, tutar, mesafe;
+        int min = 20, acilis = 10;
+
+        System.out.print("Gidilecek mesafeyi (KM) giriniz: ");
+        mesafe = inp.nextInt();
+
+        tutar = mesafe * kmUcreti + acilis;
+        tutar = (tutar < 20) ? min : tutar;
+
+        System.out.print("Toplam tutar: " + tutar);
+    }
+}
