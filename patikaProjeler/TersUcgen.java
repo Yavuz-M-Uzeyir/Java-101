ff8a2123ab0426b6518dbab689074daea1086740
üzeyir <barbaross_61@hotmail.com>
2024-04-23 18:21:13 +0300

    2

--- patikaProjeler/TersUcgen.java
+++ patikaProjeler/TersUcgen.java
@@ -10,7 +10,10 @@ public class TersUcgen {
         int n = inp.nextInt();
 
         for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= (i * 2) - 1; j++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= (2*i) - 1; k++){
                 System.out.print("*");
             }
             System.out.println();
