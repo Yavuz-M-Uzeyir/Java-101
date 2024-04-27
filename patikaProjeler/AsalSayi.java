cf39c7ea98571b3c1ae743a619b20a3f69931d3b
üzeyir <barbaross_61@hotmail.com>
2024-04-27 20:40:43 +0300

    2

--- patikaProjeler/AsalSayi.java
+++ patikaProjeler/AsalSayi.java
@@ -1,34 +1,9 @@
 package patikaProjeler;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class AsalSayi {
     public static void main(String[] args) {
-
-        ArrayList<Integer> liste = new ArrayList<>();
-        for (int i = 2; i <= 100; i++) {
-            if (i == 2) {
-                liste.add(2);
-            } else if (i == 3) {
-                liste.add(3);
-            } else if (i == 5) {
-                liste.add(5);
-            } else if (i == 7) {
-                liste.add(7);
-            } else {
-                if (i % 2 != 0) {
-                    if (i % 3 != 0) {
-                        if (i % 5 != 0) {
-                            if (i % 7 != 0) {
-                                liste.add(i);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println(liste);
-
+        
     }
 }
