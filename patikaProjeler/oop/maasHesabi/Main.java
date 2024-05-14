47abdfbd46bf49be07b96c7ca6a03036a31129e1
üzeyir <barbaross_61@hotmail.com>
2024-05-14 18:22:22 +0300

    employee added

---
+++ patikaProjeler/oop/maasHesabi/Main.java
@@ -0,0 +1,8 @@
+package patikaProjeler.oop.maasHesabi;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Burak", 4000, 40, 2021);
+        e1.toStrng();
+    }
+}
