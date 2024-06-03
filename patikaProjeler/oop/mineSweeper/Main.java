679b10f5679cd0c88d6037d094be9a2de2d051dd
üzeyir <barbaross_61@hotmail.com>
2024-06-03 16:29:37 +0300

    Mine sweeper completed

--- patikaProjeler/oop/mineSweeper/Main.java
+++ patikaProjeler/oop/mineSweeper/Main.java
@@ -1,9 +1,19 @@
 package patikaProjeler.oop.mineSweeper;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        MineSweeper game = new MineSweeper(2, 2);
+        Scanner inp = new Scanner(System.in);
+        int line, column;
 
+        System.out.print("Oyun kaç satırdan oluşsun: ");
+        line = inp.nextInt();
+
+        System.out.print("Oyun kaç stündan oluşsun: ");
+        column = inp.nextInt();
+
+        MineSweeper game = new MineSweeper(line, column);
         game.play();
     }
 }
--- patikaProjeler/oop/mineSweeper/MineSweeper.java
+++ patikaProjeler/oop/mineSweeper/MineSweeper.java
@@ -11,106 +11,118 @@ public class MineSweeper {
     String[][] map;
     int line;
     int column;
-    int mine;
     int selectLine;
     int selectColumn;
+    int mine;
 
     MineSweeper(int line, int column) {
+        this.line = line;
+        this.column = column;
+        this.mine = (this.line * this.column) / 4;
 
-        if (line >= 2 && column >= 2) {
+        this.board = new String[this.line][this.column];
+        this.map = new String[this.line][this.column];
 
-            this.line = line;
-            this.column = column;
-            this.mine = (this.line * this.column) / 4;
-
-            this.board = new String[this.line][this.column];
-            this.map = new String[this.line][this.column];
-
-            for (int i = 0; i < this.board.length; i++) {
-                for (int j = 0; j < this.board[i].length; j++) {
-                    this.board[i][j] = "-";
-                    this.map[i][j] = "-";
-                }
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[i].length; j++) {
+                this.board[i][j] = "-";
+                this.map[i][j] = "-";
             }
-        } else {
-            System.out.println("Satır ve stün 2 den az olamaz.");
         }
     }
 
     void play() {
+        if (this.line < 2 || this.column < 2) {
+            System.out.println("Satır veya stün 2 den az olamaz!");
+        } else {
+            plantMine();
+            System.out.println("<<<<Mayın tarlası oyununa hoş geldiniz>>>>");
+            int succes = 0;
+            while (true) {
+                printBoard(board);
 
-        plantMine();
-        printBoard(map);
-        System.out.println("<<<<Mayın tarlası oyununa hoş geldiniz>>>>");
-        while (!isSelectMine()) {
-            printBoard(board);
-        }
-    }
+                System.out.print("Satır : ");
+                this.selectLine = inp.nextInt();
 
-    boolean isSelectMine() {
+                System.out.print("Stün : ");
+                this.selectColumn = inp.nextInt();
 
-        System.out.print("Satır : ");
-        this.selectLine = inp.nextInt();
+                if (!this.board[this.selectLine][this.selectColumn].equals("-")) {
+                    System.out.println("Bu koordinat daha önce seçildi başka bir koordinat giriniz!");
+                    continue;
+                }
+                if (this.selectLine < 0 || selectLine >= this.line) {
+                    System.out.println("Hatalı koordinat!");
+                    continue;
+                }
+                if (this.selectColumn < 0 || selectColumn >= this.column) {
+                    System.out.println("Hatalı koordinat!");
+                    continue;
+                }
 
-        System.out.print("Stün : ");
-        this.selectColumn = inp.nextInt();
-
-        for (int i = 0; i < this.map.length; i++) {
-            for (int j = 0; j < this.map[i].length; j++) {
-                if (this.map[selectLine][selectColumn].equals("*")) {
-                    System.out.println("Oyunu kaybettiniz! Mayınların konumu : ");
-                    printBoard(map);
-                    return true;
+                if (isMineFind()) {
+                    break;
                 } else {
-                    mineNumber(i, j);
+                    succes++;
+                    mineNumber(this.selectLine, this.selectColumn);
+                    if (succes == (this.line * this.column) - this.mine) {
+                        System.out.println("Kazandınız!");
+                        printBoard(board);
+                        System.out.println("Mayınların konumu: ");
+                        printBoard(map);
+                        break;
+                    }
                 }
             }
         }
-        return false;
     }
 
-    boolean isWin() {
-
-        return false;
-    }
-
-    int mineNumber(int line, int colmn) {
-
+    int mineNumber(int l, int c) {
         int count = 0;
-        int tempCound = 0;
+        if (this.map[l][c].equals("-")) {
+            if (c - 1 >= 0 && this.map[l][c - 1].equals("*")) {
+                ++count;
+            }
+            if (l + 1 < this.line && c - 1 >= 0 && this.map[l + 1][c - 1].equals("*")) {
+                ++count;
+            }
+            if (l + 1 < this.line && this.map[l + 1][c].equals("*")) {
+                ++count;
+            }
+            if (l + 1 < this.line && c + 1 < this.column && this.map[l + 1][c + 1].equals("*")) {
+                ++count;
+            }
+            if (c + 1 < this.column && this.map[l][c + 1].equals("*")) {
+                ++count;
+            }
+            if (l - 1 >= 0 && c + 1 < this.column && this.map[l - 1][c + 1].equals("*")) {
+                ++count;
+            }
+            if (l - 1 >= 0 && this.map[l - 1][c].equals("*")) {
+                ++count;
+            }
+            if (l - 1 >= 0 && c - 1 >= 0 && this.map[l - 1][c - 1].equals("*")) {
+                ++count;
+            } else {
+                this.board[l][c] = String.valueOf(0);
+            }
+            this.board[l][c] = String.valueOf(count);
+        }
+        return count;
+    }
 
-        if ((colmn < this.column - 1) && (this.map[line][colmn - 1].equals("*"))) {
-            this.board[line][colmn] = String.valueOf(++count);
+    boolean isMineFind() {
+        if (this.map[this.selectLine][this.selectColumn].equals("*")) {
+            System.out.println("Mayına bastınız!");
+            printBoard(board);
+            System.out.println("Mayınların konumu: ");
+            printBoard(map);
+            return true;
         }
-        if (this.map[line + 1][colmn - 1].equals("*")) {
-            this.board[line][colmn] = String.valueOf(++count);
-        }
-        if (this.map[line + 1][colmn].equals("*")) {
-            this.board[line][colmn] = String.valueOf(++count);
-        }
-        if (this.map[line + 1][colmn - 1].equals("*")) {
-            this.board[line][colmn] = String.valueOf(++count);
-        }
-        if (this.map[line][colmn + 1].equals("*")) {
-            this.board[line][colmn] = String.valueOf(++count);
-        }
-        if (this.map[line - 1][colmn + 1].equals("*")) {
-            this.board[line][colmn] = String.valueOf(++count);
-        }
-        if (this.map[line - 1][colmn].equals("*")) {
-            this.board[line][colmn] = String.valueOf(++count);
-        }
-        if (this.map[line - 1][colmn - 1].equals("*")) {
-            this.board[line][colmn] = String.valueOf(++count);
-        }
-
-        tempCound = count;
-        count = 0;
-        return tempCound;
+        return false;
     }
 
     void plantMine() {
-
         int randomLine;
         int randomColumn;
         int i = 0;
@@ -125,7 +137,6 @@ public class MineSweeper {
     }
 
     void printBoard(String[][] mtrx) {
-
         for (int i = 0; i < mtrx.length; i++) {
             for (int j = 0; j < mtrx[i].length; j++) {
                 System.out.print(mtrx[i][j] + " ");
